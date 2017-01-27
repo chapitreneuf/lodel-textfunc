f5aa21ee3bd1bf53298be4234c364310942eadf1
ArnAud Cordier <arnaud@arnaudcordier.org>
2017-01-27 12:41:27 +0100

    lang_is_rtl: fonction pour définir la direction d'un texte selon sa langue

--- textfunc_local.php
+++ textfunc_local.php
@@ -29,6 +29,11 @@ function repliquer($texte, $nb) {
 	return $res;
 }
 
+function lang_is_rtl($lang) {
+	$rtl_languages = array('ar' /* 'العربية', Arabic */, 'arc' /* Aramaic */, 'bcc' /* 'بلوچی مکرانی', Southern Balochi */, 'bqi' /* 'بختياري', Bakthiari */, 'ckb' /* 'Soranî / کوردی', Sorani */, 'dv' /* Dhivehi */, 'fa' /* 'فارسی', Persian */, 'glk' /* 'گیلکی', Gilaki */, 'he' /* 'עברית', Hebrew */, 'ku' /* 'Kurdî / كوردی', Kurdish */, 'mzn' /* 'مازِرونی', Mazanderani */, 'pnb' /* 'پنجابی', Western Punjabi */, 'ps' /* 'پښتو', Pashto, */, 'sd' /* 'سنڌي', Sindhi */, 'ug' /* 'Uyghurche / ئۇيغۇرچە', Uyghur */, 'ur' /* 'اردو', Urdu */, 'yi' /* 'ייִדיש', Yiddish */);
+	return in_array($lang, $rtl_languages);
+}
+
 /**
  DOM utility functions
 */
