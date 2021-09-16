5e9dc563e365e4b805c8f5395a14f9b65e98e346
Thomas Brouard <contact@brrd.fr>
2021-09-16 14:16:57 +0200

    Fix alias

--- alias.php
+++ alias.php
@@ -5,13 +5,13 @@
  */
 
 if (!function_exists('vignettiser')) {
-	function vignettiser($width, $height) {
-		return illustrations($width, $height);
+	function vignettiser($text, $width, $height) {
+		return illustrations($text, $width, $height);
 	}
 }
 
 if (!function_exists('embedMedia')) {
-	function embedMedia($width, $onlyDesc) {
-		return media();
+	function embedMedia($text, $width = 400, $onlyDesc = false) {
+		return media($text);
 	}
 }
