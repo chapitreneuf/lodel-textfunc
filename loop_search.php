59a71ea025ed4b95a755f359961b7d2621a62c63
ArnAud Cordier <arnaud@arnaudcordier.org>
2022-01-25 14:07:07 +0100

    réparer la recherche sur qwant (nouvelle api)

--- loop_search.php
+++ loop_search.php
@@ -39,8 +39,8 @@ function loop_websearch(&$context, $funcname, $args) {
 		return;
 	}
 
-	$total = $results['total'];
-	$localcontext['total'] = $total;
+	$loop_max = $results['count'];
+	$localcontext['total'] = $results['total'];
 
 	// code avant
 	if (function_exists("code_before_$funcname"))
@@ -53,7 +53,7 @@ function loop_websearch(&$context, $funcname, $args) {
 
 		if ($count == 1 && function_exists("code_dofirst_$funcname")) {
 			call_user_func("code_dofirst_$funcname", $docontext);
-		} elseif( $count == $total && function_exists("code_dolast_$funcname") ) {
+		} elseif( $count == $loop_max && function_exists("code_dolast_$funcname") ) {
 			call_user_func("code_dolast_$funcname", $docontext);
 		} else {
 			call_user_func("code_do_$funcname", $docontext);
@@ -67,19 +67,30 @@ function loop_websearch(&$context, $funcname, $args) {
 // ask qwant
 // Output:
 // [
-//		'total' => int,
-//		'items' => [ [ 'title','url','favicon','source','desc','_id','position'], … ]
+//		'total' => (int) supposed max number of items
+//		'count' => (int) number of items
+//		'last_page' => (bool) more results or not,
+//		'items' => [ [ 'title','url','favicon','source','desc','_id'], … ]
 // ]
 function search_qwant($q, $limit, $offset, $site) {
+
+	// Limit doit être un multiple de 10, qwant n'accepte que 10
+	if (10 % $limit !== 0) {
+		trigger_error("websearch LOOP: limit argument must be a multiple of 10. $limit is not !", E_USER_ERROR);
+	}
+
 	// URL de l'API + la recherche
-	$url = 'https://api.qwant.com/api/search/web?q=site:'.$site.'+'.$q;
+	$url = 'https://api.qwant.com/v3/search/web?q=site:'.$site.'+'.$q;
 
 	// ajout des paramètres (non docummentés) pour qwant
-	$url .= "&count=$limit&t=web&extensionDisabled=true&safesearch=1&locale=fr_FR&uiv=4";
+	$url .= "&count=10&safesearch=1&locale=fr_FR&device=desktop";
 
 	// ajout de la pagination
-	if ($offset)
-		$url .= "&offset=$offset";
+	if ($offset) {
+		$page = floor($offset/10) * 10;
+		if ($page)
+			$url .= "&offset=$page";
+	}
 
 	// chargment de la réponse
 	$ret = curl_get($url);
@@ -92,11 +103,32 @@ function search_qwant($q, $limit, $offset, $site) {
 	$json = json_decode($ret, true);
 	if (!$json || empty($json['data']['result'])) {
 		error_log("Pb avec qwant $url." . var_export($ret, true));
-		return array('total'=>0,'items'=>[]);
+		return array('total'=>0, 'items'=>[], 'count'=>0, 'last_page'=>1);
 	}
 
-	$results = $json['data']['result'];
-	unset($results['filters']);
+	$results = $json['data']['result']['items']['mainline'];
+	foreach( $json['data']['result']['items']['mainline'] as $c ) {
+		if($c['type'] == "web") {
+			$results = $c;
+			break;
+		}
+	}
+	$qwant_items = sizeof($results['items']);
+
+	// tailler les résultats pour correspondre à limit et offset
+	$start = $offset % 10;
+	$results['items'] = array_slice($results['items'], $start, $limit);
+	$results['count'] = sizeof($results['items']);
+
+	// We don't have real total nor last_page
+	// Trick loop total to be bigger if not on last page
+	$results['last_page'] =  $qwant_items < 10 && $start + $limit >= $qwant_items;
+	$results['total'] = $offset + $results['count'];
+	if (!$results['last_page']) {
+		$results['total'] += 1;
+	}
+
+	// error_log('Results: ' . var_export($results, 1));
 
 	return $results;
 }
